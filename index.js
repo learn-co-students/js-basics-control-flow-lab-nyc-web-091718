3833b29740a8cf7d8c18dbc50478b4e11ee109d2
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-28 17:33:09 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let message;
+  if (feet <= 400) {
+      message = "This one is on me!";
+  } else if (feet > 2500) {
+      message = "No can do.";
+  } else {
+      message = "I will gladly take your thirty bucks.";
+  }
+  return message
+}
+
+function ternaryCheckCity(city) {
+  var message2 = "NYC" === city ? "Ok, sounds good." : "No go.";
+  return message2
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+        return 'Thank you so much.';
+        break;
+    case "not as generous":
+        return 'Thank you.';
+        break;
+    default:
+        return 'Bye.'
+  }
+}
