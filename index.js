2a47b051d06339d5ee89569cf93c58efc628164f
zhenwu93 <zhenwu93@gmail.com>
2018-10-29 08:42:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code in this file!
+
+function scuberGreetingForFeet(someValue) {
+  let result;
+  if (someValue > 2500) {
+    result = "No can do."
+  } else if (someValue > 2000) {
+    result = "I will gladly take your thirty bucks."
+  } else {
+    result = "This one is on me!"
+  }
+
+  return result;
+}
+
+
+function ternaryCheckCity(city) {
+
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+}
